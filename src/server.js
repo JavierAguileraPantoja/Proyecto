9d44d19f346f7b307336ab2baa918e250e6effe5
Tarsicio Ramos <t.ramos@ugto.mx>
2023-10-19 23:18:16 -0600

    5.- Handlebars

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 //Initializations
@@ -7,15 +8,23 @@ const app = express();
 //Settings
 app.set('port',process.env.PORT||3000);
 app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs',exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine','.hbs');
 
 //Middlewares
 app.use(express.urlencoded({extended:false}));
 //Gloval Values
 
 //Routes
-app.get('/',(req, res)=>{
-res.send('Pagina Home');
+app.get('/', (req, res) => {
+    res.render('index');
 });
+
 
 //Static Files
 app.use(express.static(path.join(__dirname,'public')));
