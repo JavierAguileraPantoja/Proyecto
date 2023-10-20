cfb1c7890023d56275a416d6ed330688544ef915
Tarsicio Ramos <t.ramos@ugto.mx>
2023-10-19 22:59:30 -0600

    3.-Base de Datos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
 const app = require('./server');
+require('./databse');
+
+//console.log(process.env.TESTING);
 
 app.listen(app.get('port'),()=>{
     console.log('Servidor en Puerto',app.get('port'));
