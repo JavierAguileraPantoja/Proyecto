3969906392026cdde886f1e0fe14915cb7c54da3
Tarsicio Ramos <t.ramos@ugto.mx>
2023-10-21 22:26:04 -0600

    12.- Update Notes

--- src/controllers/notes.js
+++ src/controllers/notes.js
@@ -5,30 +5,34 @@ notesController.renderNoteForm = (req, res) => {
     res.render('notes/new-note');
 };
 
-notesController.createNewNote = async(req, res) => {
+notesController.createNewNote = async (req, res) => {
     const { title, description } = req.body;
     const newNote = new Note({ title, description });
     //console.log(newNote);
-     await newNote.save();
+    await newNote.save();
     res.redirect('/notes')
 };
 
-notesController.renderNotes = async(req, res) => {
+notesController.renderNotes = async (req, res) => {
     const notes = await Note.find().lean();
-    res.render('notes/all-notes',{notes});
+    res.render('notes/all-notes', { notes });
 };
 
-notesController.renderEditForm = (req, res) => {
-    res.send('Edit Form Notes');
+notesController.renderEditForm = async (req, res) => {
+    const note = await Note.findById(req.params.id).lean();
+    //console.log(note);
+    res.render('notes/edit-note', { note });
 };
 
-notesController.updateNote = (req, res) => {
-    res.send('Update Notes');
+notesController.updateNote = async (req, res) => {
+    const { title, description } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title, description });
+    res.redirect('/notes');
 };
 
-notesController.deleteNote = async(req, res) => {
+notesController.deleteNote = async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
-    res.redirect('/notes')
+    res.redirect('/notes');
 };
 
 
