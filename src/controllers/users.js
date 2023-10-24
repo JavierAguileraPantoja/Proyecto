9f962a48cc7d9cfb58edf62a762e63df11980eea
Tarsicio Ramos <t.ramos@ugto.mx>
2023-10-24 14:37:17 -0600

    15.- SignUp

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,22 +1,49 @@
 const usersController = {};
+const User = require('../medels/User');
 
-usersController.renderSignUpForm=(req, res)=>{
+usersController.renderSignUpForm = (req, res) => {
     res.render('users/signup');
 };
-usersController.signup=(req,res)=>{
-    res.send('Pagina de SignUp');
+usersController.signup = async (req, res) => {
+    const errors = [];
+    const { name, email, password, confirm_password } = req.body;
+
+    if (password != confirm_password) {
+        errors.push({ text: 'Password y Confirmar Pasword No coinciden' });
+    }
+
+    if (password.length < 4) {
+        errors.push({ text: 'Password requiere minimo 4 caracteres' });
+    }
+
+    if (errors.length > 0) {
+        res.render('users/signup', { errors, name, email });
+    } else {
+        //res.send('Acceso Satisfactorio');
+        const emailUser = await User.findOne({ email: email });
+        if (emailUser) {
+            req.flash('error_msg', 'El email ya se encuntra registrado.');
+            res.redirect('/users/signup');
+        } else {
+            const newUser = new User({ name, email, password });
+            newUser.password =  await newUser.encryptPassword(password)
+            await newUser.save();
+            req.flash('success_msg','Usuario Registrado con Exito');
+            res.redirect('/users/signin');
+        }
+    }
 };
 
-usersController.renderSignInForm=(req, res)=>{
+usersController.renderSignInForm = (req, res) => {
     res.render('users/signin');
 };
-usersController.signin=(req, res)=>{
+usersController.signin = (req, res) => {
     res.send('Pagina de Signin');
 };
 
-usersController.logout=(req, res)=>{
+usersController.logout = (req, res) => {
     res.send('Pagina de Logout');
 };
 
 
-module.exports= usersController;
+module.exports = usersController;
--- src/server.js
+++ src/server.js
@@ -33,6 +33,7 @@ app.use(flash());
 //Gloval Values
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
     next();
 });
 //Routes
