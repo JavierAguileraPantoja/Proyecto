b085edd95f36d58aad66632e59e297d9a5ff6b41
Tarsicio Ramos <t.ramos@ugto.mx>
2023-10-27 22:40:22 -0600

    17.-  Proteger Rutas

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,20 +1,20 @@
 const { Router } = require('express');
 const router = Router();
 const { renderNoteForm, createNewNote, renderNotes, renderEditForm, updateNote, deleteNote } = require('../controllers/notes');
-
+const { isAuthenticated } = require('../helpers/validate');
 
 //new note
-router.get('/notes/add', renderNoteForm);
-router.post('/notes/new-note', createNewNote);
+router.get('/notes/add', isAuthenticated, renderNoteForm);
+router.post('/notes/new-note',isAuthenticated, createNewNote);
 
 //get all notes
-router.get('/notes', renderNotes);
+router.get('/notes', isAuthenticated,renderNotes);
 
 //Edit Notes
-router.get('/notes/edit/:id', renderEditForm);
-router.put('/notes/edit/:id', updateNote);
+router.get('/notes/edit/:id',isAuthenticated, renderEditForm);
+router.put('/notes/edit/:id',isAuthenticated, updateNote);
 
 //Delete Notes
-router.delete('/notes/delete/:id', deleteNote);
+router.delete('/notes/delete/:id',isAuthenticated, deleteNote);
 
 module.exports = router;
