642a63d09b7f9ba4ff730c50783d6ea82e9dd5f9
Tarsicio Ramos <t.ramos@ugto.mx>
2023-10-27 23:22:56 -0600

    18.- Notas por usuario

--- src/medels/Note.js
+++ src/medels/Note.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose');
 const NoteSchema = new Schema({
     title: { type: String, require: true },
     description: { type: String, require: true },
+    user:{type: String, require:true}
 },
     { timestamps: true }
 )
